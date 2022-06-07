060b198cbdcd0cf309a64f0447f637e35a8d131d
hwang <hhs1992@naver.com>
2022-06-08 07:26:42 +0900

    updated

--- SheetLoader/Program.cs
+++ SheetLoader/Program.cs
@@ -19,13 +19,9 @@ namespace SheetLoader
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             UserCredential credential;
             using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
             {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
                 string credPath = "token.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.FromStream(stream).Secrets,
@@ -33,7 +29,6 @@ namespace SheetLoader
                     "user",
                     CancellationToken.None,
                     new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
             }
 
             // Create Google Sheets API service.
@@ -46,8 +41,8 @@ namespace SheetLoader
             var spreadsheetId = "1vcv6nDsn7EUuyaE1U93mMVfmGJT3m-suv-CJOMYSo38";
             var a = service.Spreadsheets.Get(spreadsheetId);
             var b = a.Execute();
-            
-            foreach(var sheet in b.Sheets)
+
+            foreach (var sheet in b.Sheets)
             {
                 var c = service.Spreadsheets.Values.Get(spreadsheetId, sheet.Properties.Title);
                 var d = c.Execute();
@@ -59,8 +54,29 @@ namespace SheetLoader
         {
             var arr = new JArray();
             var headers = new Dictionary<int, string>();
-            
+            var rowIndex = 0;
+
+            if (!TrySkipRow(data, ref rowIndex)) return null;
+
+            for (int i = 0; i < data[rowIndex].Count; i++)
+            {
+                // var str = data
+            }
+
             return default;
+        }
+
+        public static bool TrySkipRow(IList<IList<object>> data, ref int index)
+        {
+            for (int i = index; i < data.Count; i++)
+            {
+                var row = data[i];
+                var str = row.Count > 0 ? row[0]?.ToString() : null;
+                if (string.IsNullOrWhiteSpace(str) || str.StartsWith("#")) continue;
+                index = i;
+                return true;
+            }
+            return false;
         }
     }
 }
