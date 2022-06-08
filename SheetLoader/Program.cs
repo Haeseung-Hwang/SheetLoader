fb7998bdcdf7f4dcffdf007409ceb7c3239b09e0
hwang <hhs1992@naver.com>
2022-06-08 09:08:57 +0900

    load sheet

--- SheetLoader/Program.cs
+++ SheetLoader/Program.cs
@@ -41,13 +41,17 @@ namespace SheetLoader
             var spreadsheetId = "1vcv6nDsn7EUuyaE1U93mMVfmGJT3m-suv-CJOMYSo38";
             var a = service.Spreadsheets.Get(spreadsheetId);
             var b = a.Execute();
-
+            var rootJson = new JObject();
+            var dataObj = new JObject();
             foreach (var sheet in b.Sheets)
             {
                 var c = service.Spreadsheets.Values.Get(spreadsheetId, sheet.Properties.Title);
                 var d = c.Execute();
                 var e = d.Values;
+                dataObj.Add(sheet.Properties.Title, ToArray(e));
             }
+            rootJson.Add("Data", dataObj);
+            File.WriteAllText("../Output.json", rootJson.ToString(Newtonsoft.Json.Formatting.Indented));
         }
 
         public static JArray ToArray(IList<IList<object>> data)
@@ -56,14 +60,37 @@ namespace SheetLoader
             var headers = new Dictionary<int, string>();
             var rowIndex = 0;
 
-            if (!TrySkipRow(data, ref rowIndex)) return null;
+            // if (!TrySkipRow(data, ref rowIndex)) return null;
 
             for (int i = 0; i < data[rowIndex].Count; i++)
             {
                 // var str = data
+                var str = data[rowIndex][i]?.ToString();
+                if(string.IsNullOrWhiteSpace(str) || str.StartsWith("#")) continue;
+                headers.Add(i, str);
             }
 
-            return default;
+            rowIndex++;
+
+            while (TrySkipRow(data, ref rowIndex))
+            {
+                var elem = new JObject();
+                foreach (var kv in headers)
+                {
+                    var row = data[rowIndex];
+                    if (kv.Key >= row.Count) continue;
+                    var value = row[kv.Key];
+                    if ((string)value == string.Empty) continue;
+                    elem.Add(kv.Value, new JValue(value));
+                }
+                arr.Add(elem);
+                rowIndex++;
+            }
+
+            if (arr.Count == 0) return null;
+
+
+            return arr;
         }
 
         public static bool TrySkipRow(IList<IList<object>> data, ref int index)
